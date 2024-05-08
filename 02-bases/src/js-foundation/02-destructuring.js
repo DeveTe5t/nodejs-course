56f86876626fc5ab950bb558bb697e0eb518d0c4
DeveTe5t <devcurses@gmail.com>
2024-05-08 14:30:02 -0600

    Debug with break point

--- 02-bases/src/js-foundation/02-destructuring.js
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -5,6 +5,6 @@
 const { SHELL, LOGNAME } = process.env;
 // console.table({ SHELL, LOGNAME });
 
-const characters = ['Flash', 'Superman', 'Batman'];
-const [flash, , batman] = characters;
+const characters = ['Flash', 'Superman', 'Green Lantern', 'Batman'];
+const [flash, , , batman] = characters;
 // console.log(flash, batman);
