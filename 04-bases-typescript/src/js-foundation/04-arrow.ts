5e02409e9abae644f7f02519ed0adcb6f15afdde
DeveTe5t <devcurses@gmail.com>
2024-05-23 12:57:34 -0600

    Changes for rename folder

---
+++ 04-bases-typescript/src/js-foundation/04-arrow.ts
@@ -0,0 +1,35 @@
+interface User {
+    id: number;
+    name: string;
+}
+
+const users: User[] = [
+    {
+        id: 1,
+        name: 'John Doe',
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+    }
+];
+
+export function getUserById(id: number, callback: (error?: string, user?: User) => void) {
+    // const user = users.find(user => user.id === id);
+    const user = users.find(function (user) {
+        return user.id === id;
+    });
+
+    if (!user) {
+        return callback(`User not found with id ${id}`);
+    }
+
+    return callback(undefined, user);
+}
+
+export const getUserByIdArrow = (id: number, callback: (error?: string, user?: User) => void) => {
+    const user = users.find(user => user.id === id);
+
+    // Return is not necessary
+    user ? callback(undefined, user) : callback(`User not found with id ${id}`);
+}
