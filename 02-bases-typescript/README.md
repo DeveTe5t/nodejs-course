5548e23d9c98df09a08ac3959dbbd5ca9845cb0e
DeveTe5t <devcurses@gmail.com>
2024-05-23 13:00:10 -0600

    Deleted files for rename folder from 02-bases-typescript to 04-bases-typescript

--- 02-bases-typescript/README.md
+++
@@ -1,7 +0,0 @@
-# 02 Node bases
-
-
-Run in dev
-```
-npm run dev
-```
--- 02-bases-typescript/src/js-foundation/01-template.ts
+++
@@ -1,7 +0,0 @@
-export const emailTemplate = `
-    <div>
-        <h1>Hi, {{name}}</h1>
-        <p>Thank you for your interest in our product.</p>
-    </div>`;
-
-// console.log(emailTemplate);
