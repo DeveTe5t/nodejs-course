2a088b6f91b78167f7f2f631c41ce651fd2ee669
DeveTe5t <devcurses@gmail.com>
2024-05-27 10:16:55 -0600

    Added promises test

--- 04-bases-typescript/src/js-foundation/06-promises.ts
+++ 04-bases-typescript/src/js-foundation/06-promises.ts
@@ -1,4 +1,4 @@
-import { http } from '../plugins';
+import { http } from '../plugins'; // pass for index and there is http exported 
 
 export const getPokemonById = async (id: string | number): Promise<string> => {
     // return new Promise((resolve, reject) => {
@@ -8,16 +8,20 @@ export const getPokemonById = async (id: string | number): Promise<string> => {
     //         .catch(error => reject(error));
     // });
 
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    try {
 
-    // const response = await fetch(url);
-    // const pokemon = await response.json();
-
-    const pokemon = await http.get(url);
-
-    // throw new Error('Pokemon not exists!');
-    return pokemon.name;
-
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        // const response = await fetch(url);
+        // const pokemon = await response.json();
+        const pokemon = await http.get(url);
+        return pokemon.name;
+    }
+    catch (error) {
+        // way 1
+        throw (`Pokemon not found with id ${id}`);
+        // way 2
+        // throw new Error(`Pokemon not found with id ${id}`);
+    }
     // // Way 3
     // return fetch(url)
     //     .then((response) => response.json())
---
+++ 04-bases-typescript/tests/js-foundation/06-promises.test.ts
@@ -0,0 +1,37 @@
+import { getPokemonById } from '../../src/js-foundation/06-promises';
+
+describe('js-foundation/06-promises.ts', () => {
+
+    test('getPokemonById should return a pokemon', async () => {
+
+        const pokemonId = 1;
+        const pokemonName = await getPokemonById(pokemonId);
+        expect(pokemonName).toBe('bulbasaur');
+    });
+
+    // way 1
+    test('getPokemonById should return an error if pokemon not exist', async () => {
+
+        const pokemonId = 1000000000;
+
+        try {
+
+            await getPokemonById(pokemonId);
+            expect(true).toBeFalsy();
+        }
+        catch (error) {
+
+            expect(error).toBe(`Pokemon not found with id ${pokemonId}`);
+        }
+    });
+
+    // way 2
+    // test("getPokemonById should return an error if pokemon not exist 2", async () => {
+
+    //     const pokemonId = 1000000000;
+
+    //     await expect(getPokemonById(pokemonId)).rejects.toThrow(        
+    //         `Pokemon not found with id ${pokemonId}`
+    //     );
+    // });
+});
