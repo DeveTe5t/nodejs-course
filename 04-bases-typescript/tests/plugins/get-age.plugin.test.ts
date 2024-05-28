022af2c98465fa4ab261e2abb42731ca9de65c03
DeveTe5t <devcurses@gmail.com>
2024-05-28 09:21:41 -0600

    Added get-age.plugins test

---
+++ 04-bases-typescript/tests/plugins/get-age.plugin.test.ts
@@ -0,0 +1,33 @@
+import { getAge } from "../../src/plugins";
+
+describe('plugins/get-age.plugin.ts', () => {
+
+    test('getAge should return the age of a person', () => {
+
+        const birthdate = '1980-12-12';
+        const age = getAge(birthdate);
+
+        // expect(age).toBe(44);
+        expect(typeof age).toBe('number');
+    });
+
+    test('getAge should return current age', () => {
+
+        const birthdate = '1980-12-12';
+        const age = getAge(birthdate);
+
+        const calculatedAge = new Date().getFullYear() - new Date(birthdate).getFullYear();
+        expect(age).toBe(calculatedAge);
+    });
+
+    test('getAge should return 0 years', () => {
+
+        const spy = jest.spyOn(Date.prototype, 'getFullYear').mockReturnValue(1980);
+
+        const birthdate = '1980-12-12';
+        const age = getAge(birthdate);
+
+        expect(age).toBe(0);
+        expect(spy).toHaveBeenCalled();
+    });
+});
