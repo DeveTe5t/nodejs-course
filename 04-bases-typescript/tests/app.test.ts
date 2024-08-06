a062eaf3b62722259adf8bbe59f66b11c02e451a
DeveTe5t <devcurses@gmail.com>
2024-08-06 13:16:39 -0600

    Add comments to app.test.ts in 04-bases-typescript

--- 04-bases-typescript/tests/app.test.ts
+++ 04-bases-typescript/tests/app.test.ts
@@ -5,14 +5,14 @@ describe('Test in the App File', () => {
     // it('should be true', () => {
     test('should be 30', () => {
 
-        // 1. Arrage
+        // 1. Arrage: initialization variables, etc.
         const number1 = 10;
         const number2 = 20;
 
-        // 2. Act
+        // 2. Act: call variables, functions, etc.
         const result = number1 + number2;
 
-        // 3. Assert
+        // 3. Assert: verify results
         expect(result).toBe(30);
     });
 });
