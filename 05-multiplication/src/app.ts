429a43d3c37281f23104382f04fa9bffff6a7305
DeveTe5t <devcurses@gmail.com>
2024-08-13 14:36:11 -0600

    start argv

--- 05-multiplication/src/app.ts
+++ 05-multiplication/src/app.ts
@@ -1,39 +1,7 @@
-// way 1 mine
-// import { header } from './header';
-// import { contentBody } from './content';
-// import { createFile } from './create-file';
 
-// const base = 5;
 
-// const title = header(`Table of ${base}`);
-// const multiplication = contentBody(base);
-// createFile(`table-${base}.txt`, `${title}\r${multiplication}`);
+console.log(process.argv);
 
-// console.log(title);
-// console.log(multiplication);
+const [tsnode, app, ...args] = process.argv;
 
-// way 2
-import fs from 'fs';
-
-let outputMessage = '';
-const base = 9;
-const headerMessage = `
-=================================
-            Table of ${base}
-=================================\n
-`;
-
-for (let i = 1; i <= 10; i++) {
-    (i < 10)
-        ? outputMessage += `${base} x ${i}  = ${base * i}\n`
-        : outputMessage += `${base} x ${i} = ${base * i}`;
-}
-
-outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
-
-const outputPath = 'outputs';
-
-fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${outputPath}/table-${base}.txt`, outputMessage);
-console.log('File created!');
+console.log(args);
