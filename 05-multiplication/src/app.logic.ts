1bbfdb1552c1ca039b6fa45389858291f47b2651
DeveTe5t <devcurses@gmail.com>
2024-08-14 10:27:56 -0600

    Mutiplication table with params base, limit, show pass by console

--- 05-multiplication/src/app.logic.ts
+++ 05-multiplication/src/app.logic.ts
@@ -14,26 +14,48 @@
 
 // way 2
 import fs from 'fs';
+import { yarg } from './config/plugins/args.plugin';
+
+const { b: baseMultiplication, l: limitMultiplication, s: showTableMultiplication } = yarg;
 
 let outputMessage = '';
-const base = 9;
+// const base = 9;
+// const base = yarg.b;
+// const limit = yarg.l;
+// const show = yarg.s;
 const headerMessage = `
 =================================
-            Table of ${base}
+            Table of ${baseMultiplication}
 =================================\n
 `;
 
-for (let i = 1; i <= 10; i++) {
-    (i < 10)
-        ? outputMessage += `${base} x ${i}  = ${base * i}\n`
-        : outputMessage += `${base} x ${i} = ${base * i}`;
+const spaceBaseBetweenEqual = (iteration: number) => {
+
+    let space = '';
+    for (let i = iteration.toString().length; i < limitMultiplication.toString().length; i++) {
+        space += ' ';
+    }
+
+    return space;
+};
+
+// for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= limitMultiplication; i++) {
+    // (i < 10)
+    // (i < limit)    
+    //     ? outputMessage += `${base} x ${i}  = ${base * i}\n`
+    //     : outputMessage += `${base} x ${i} = ${base * i}`;
+
+    outputMessage += `${baseMultiplication} x ${i}${spaceBaseBetweenEqual(i)} = ${baseMultiplication * i}\n`;
 }
 
 outputMessage = headerMessage + outputMessage;
-console.log(outputMessage);
+if (showTableMultiplication) {
+    console.log(outputMessage);
+}
 
 const outputPath = 'outputs';
 
 fs.mkdirSync(outputPath, { recursive: true });
-fs.writeFileSync(`${outputPath}/table-${base}.txt`, outputMessage);
+fs.writeFileSync(`${outputPath}/table-${baseMultiplication}.txt`, outputMessage);
 console.log('File created!');
--- 05-multiplication/src/app.ts
+++ 05-multiplication/src/app.ts
@@ -1,7 +1,16 @@
+import { yarg } from "./config/plugins/args.plugin";
 
+// console.log(process.argv);
+// const [tsnode, app, ...args] = process.argv;
+// console.log(args);
 
-console.log(process.argv);
+// console.log(yarg.b);
 
-const [tsnode, app, ...args] = process.argv;
+(async () => {
+    await main();
+})();
 
-console.log(args);
+async function main() {
+
+    console.log(yarg);
+}
