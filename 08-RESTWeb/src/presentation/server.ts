4e91902c55673011a34ae5a1f39b20baca15f29c
DeveTe5t <devcurses@gmail.com>
2024-09-23 10:55:06 -0600

    Start restserver

--- 08-RESTWeb/src/presentation/server.ts
+++ 08-RESTWeb/src/presentation/server.ts
@@ -1,31 +1,38 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
 export class Server {
 
+    // "express": "^5.0.0"
     private app = express();
     private readonly port: number;
+    private readonly routes: Router;
     private readonly publicPath: string;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
 
         this.port = port;
+        this.routes = routes;
         this.publicPath = public_path;
     }
 
     async start() {
         // * Middleware
 
-        // * Public folder
-        // "express": "^5.0.0"
+        // * Public folder        
         this.app.use(express.static(this.publicPath));
 
+        //* Routes
+        this.app.use(this.routes);
+
+        //* SPA
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
---
+++ 08-RESTWeb/src/presentation/todos/routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { TodosController } from "./controller";
+
+
+export class TodoRoutes {
+
+    static get routes(): Router {
+        const router = Router();
+        const todoController = new TodosController();
+
+        // router.get('/api/todos', (req, res) => todoController.getTodos(req, res));
+        // similar but not necesary because is parametes in the same order
+        router.get('/', todoController.getTodos);
+
+        return router;
+    }
+}
